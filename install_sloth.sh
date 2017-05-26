258398bbe2e9ae8218b455051d3a8c7bc9de0f6f
Qiankun Miao <qmiao@nullmaxtech.com>
2017-05-26 14:44:51 +0800

    Clean up dead code and detect whoami

--- install_sloth.sh
+++ install_sloth.sh
@@ -1,21 +1,14 @@
 #apt-get install vim git openssh-server
+sudo apt-get update
 
 bash Anaconda2-4.3.1-Linux-x86_64.sh
-conda install pyqt=4
-conda install pil
-
-#wget http://effbot.org/downloads/Imaging-1.1.7.tar.gz
-#tar zxvf Imaging-1.1.7.tar.gz
-#cd Imaging-1.1.7/
-#python setup.py install
-#sudo python setup.py install
-#cd ..
-#rm Imaging-1.1.7 -r
-
-#git clone https://github.com/cvhciKIT/sloth.git
+conda install pyqt=4 pil
+. ~/.bashrc
 
 cd sloth
 python setup.py install
 cd ..
 
-sudo ln -sf /home/user01/anaconda2/bin/python /usr/bin
+user=`whoami`
+pythondir="/home/"$user"/anaconda2/bin/python"
+sudo ln -sf $pythondir /usr/bin
